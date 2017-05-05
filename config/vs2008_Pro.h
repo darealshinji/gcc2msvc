052a0b128aa987c526159fcb2ce78252716d40ff
darealshinji <djcj@gmx.de>
2017-05-05 12:18:49 +0200

    Instructions how to make vs2008 cl.exe work

--- config/vs2008_Pro.h
+++ config/vs2008_Pro.h
@@ -6,6 +6,11 @@
  "/I'C:/Program Files/Microsoft SDKs/Windows/v6.0A/Include'"
 
 #define DEFAULT_CL_CMD_X64 "/mnt/c/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/amd64/cl.exe"
+
+/* You need to copy the following files from "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE"
+ * to "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin" to ensure the x86 cl.exe
+ * binary can be run from Bash:
+ * msdis160.dll msobj80.dll mspdb80.dll mspdbcore.dll mspdbsrv.exe */
 #define DEFAULT_CL_CMD_X86 "/mnt/c/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/cl.exe"
 
 #define DEFAULT_LIBPATHS_X64 \
