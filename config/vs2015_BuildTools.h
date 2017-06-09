fd71ce0d587eedba36f6c0ad8aeb31ef8e447533
darealshinji <djcj@gmx.de>
2017-06-09 22:11:09 +0200

    Update vs2015_BuildTools.h

--- config/vs2015_BuildTools.h
+++ config/vs2015_BuildTools.h
@@ -11,8 +11,8 @@
  "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64"
 
 #define DEFAULT_CL_CMD_X86 \
- "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64;"\
- "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64_x86"
+ "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64_x86;"\
+ "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64"
 
 #define DEFAULT_LIBPATHS_X64 \
  "/libpath:'C:/Program Files (x86)/Windows Kits/10/Lib/10.0.14393.0/ucrt/x64' " \
